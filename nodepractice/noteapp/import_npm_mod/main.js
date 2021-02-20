d1a2d6c62e598f19d6f9df0733a94f02fb6a2c44
cortesleonardo11 <32949936+cortesleonardo11@users.noreply.github.com>
2021-02-20 03:02:19 -0600

    Add files via upload

--- nodepractice/noteapp/import_npm_mod/main.js
+++ nodepractice/noteapp/import_npm_mod/main.js
@@ -1,13 +1,3 @@
-// here we call the package we installed in the terminal
-const validator = require(`validator`);
+const chalk = require('chalk');
 
-// were calling in function in validator and displaying result
-// check if email is valid, will print true
-console.log(validator.isEmail('cortesleonardo333@gmail.com'));
-
-// produce false
-console.log(validator.isEmail('tdhnei12345'));
-
-//another example using validator
-//test if input is URL
-console.log(validator.isURL('https://www.npmjs.com/package/validator'));
+console.log(chalk.red.bold.italic("success!"));
