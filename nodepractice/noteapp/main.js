1fa2826d481e0d7c2b94449e63fc2619d7282fe4
cortesleonardo11 <32949936+cortesleonardo11@users.noreply.github.com>
2021-02-20 23:56:01 -0600

    Add files via upload

--- nodepractice/noteapp/main.js
+++ nodepractice/noteapp/main.js
@@ -1,10 +1,56 @@
-// this is how we can get user input in the terminal
-//we can terminal type(node main.js leo) recieve leo as input
-// we start from the secong position to omit all the unneccesary string 
-console.log(process.argv[2]);
+// we installed yargs package to help with input
+// call yargs
+const yargs = require(`yargs`);
 
-// we make userinput = to command to use it
-const command = process.argv[2];
+// how we would originally get itput in terminal
+//console.log(process.argv[2]);
 
-if (command === 'add') console.log(`adding notes test`);
-else if (command === 'remove') console.log(`removing notes test`);
+//using yargs to get terminal input 
+//yargs will parse the package for me 
+// node main.js add title:'things to buy'
+//console.log(yargs.argv);// this is moved to the bottom
+
+//node main.js --help to get help menu with args
+
+// from here we build our command line to run commands
+
+yargs.version('1.1.0');
+
+//add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function() {
+        console.log(`adding note`);
+    }
+})
+
+// remove command
+yargs.command({
+    command: `remove`,
+    describe: `remove notes`,
+    handler: function() {
+        console.log(`remove note`)
+    }
+})
+
+//read
+yargs.command({
+    command: 'read',
+    describe: 'read from notes',
+    handler: function() {
+        console.log('read notes');
+    }
+})
+
+//list
+yargs.command({
+    command: 'list',
+    describe: 'list ',
+    handler: function() {
+        console.log('list out');
+    }
+})
+
+//moved to the bottom because program would not function correctly other wise
+console.log(yargs.argv);
