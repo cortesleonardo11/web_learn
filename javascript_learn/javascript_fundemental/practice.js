08b58af195d775f517eefd4faebcb2c26c24bd5b
cortesleonardo11 <32949936+cortesleonardo11@users.noreply.github.com>
2022-08-16 12:57:59 -0500

    Add files via upload

---
+++ javascript_learn/javascript_fundemental/practice.js
@@ -0,0 +1,10 @@
+
+
+
+console.log("values and variables");
+var num1 = 1;
+let num2 = 2; 
+const num3 = 3;
+num4 = 4; 
+
+console.log(`num1 = ${num1}, num2 = ${num2}, num3 = ${num3}, num4 = ${num4}`); 
