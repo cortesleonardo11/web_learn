343724f18a7a00ea6006e26429280f6b6e115c87
cortesleonardo11 <32949936+cortesleonardo11@users.noreply.github.com>
2022-08-16 19:07:49 -0500

    Add files via upload

--- javascript_learn/javascript_fundemental/practice.js
+++ javascript_learn/javascript_fundemental/practice.js
@@ -5,7 +5,7 @@ const ageObject =
 {
     Mybirthyear: 1998,
     // here we create a function insed of an object
-    age: function calage(genBirthYear)
+    calcAge: function(genBirthYear)
     {
         return 2022 - genBirthYear;
     }
@@ -13,8 +13,10 @@ const ageObject =
 
 // we call the function inside the object using
 // the dot notation
-console.log(ageObject.age(1998));
+console.log(ageObject.calcAge(1998));
 //bracket notation
-console.log(ageObject['age'](1998));
+console.log(ageObject['calcAge'](1998));
 
 
+// this is calling the method that it is located in
+// so it can be calling local method or global method
