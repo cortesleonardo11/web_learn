e43901d1946314f32946bebab6024196603e73c0
cortesleonardo11 <32949936+cortesleonardo11@users.noreply.github.com>
2022-08-17 10:27:31 -0500

    Add files via upload

--- javascript_learn/javascript_fundemental/practice.js
+++ javascript_learn/javascript_fundemental/practice.js
@@ -1,19 +1,22 @@
 
 
-// this is calling the method that it is located in
-// this can be calling local method or global method
-// this helps with the dont repeat yourself principal
+
 const ageObject = 
 {
     Mybirthyear: 1998,
     // here we create a function inside of an object
     calcAge: function()
     {
-        return 2022 - this.Mybirthyear;// this points to ageObject
+        //create an elment age that pertains to ageObject
+        //calculate age and return age, age is now part of object elements
+        this.age = 2021 - this.Mybirthyear;
+        return this.age;
     }
-}
+};
 
-// we call the function inside the object using
-// the dot notation
+
 console.log(ageObject.calcAge());
+//here now we call the new element created, age 
+console.log(ageObject.age);
+
 
