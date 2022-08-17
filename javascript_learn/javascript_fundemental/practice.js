0df774d279a720bf66520faf9f862aee1705b68e
cortesleonardo11 <32949936+cortesleonardo11@users.noreply.github.com>
2022-08-17 14:01:01 -0500

    Add files via upload

--- javascript_learn/javascript_fundemental/practice.js
+++ javascript_learn/javascript_fundemental/practice.js
@@ -20,3 +20,28 @@ console.log(ageObject.calcAge());
 console.log(ageObject.age);
 
 
+// continue and break examples
+
+//how to pass an array into a function
+const grades = [100,75,85,88,99];
+console.log(caltotal(grades));//pass grades[] into function
+
+function caltotal(num)// calculates the total of array
+{
+    total = 0;
+
+    for(let count = 0; count < num.length; count++)
+    {
+        total = total + num[count];
+    }
+    return total;
+};
+
+
+
+/////////////////////idea section for later//////////
+// pass 2 or more arrays into a function.
+//
+//
+
+
